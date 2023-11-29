6bc951b2b0d547e92e453f32e837e35a8f564e9c
mellivorines <lilinxi015@163.com>
2023-11-29 23:45:41 +0800

    😃 初始异常

---
+++ mellivorines-blog-springboot/src/main/kotlin/io/github/mellivorines/blog/exception/BizException.kt
@@ -0,0 +1,26 @@
+package io.github.mellivorines.blog.exception
+
+import io.github.mellivorines.blog.constant.StatusCodeEnum
+
+
+/**
+ * @Description:
+ *
+ * @author lilinxi
+ * @version 1.0.0
+ * @since 2023/8/19
+ */
+ class BizException() : RuntimeException() {
+    private var code: Int = StatusCodeEnum.FAIL.code
+
+    override var message: String? = null
+
+     constructor(message: String?) : this() {
+        this.message = message
+    }
+
+     constructor(statusCodeEnum: StatusCodeEnum) : this() {
+        this.code = statusCodeEnum.code
+        message = statusCodeEnum.desc
+    }
+}
