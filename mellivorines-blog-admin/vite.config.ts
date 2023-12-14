bd70757a1e7e7bdf430453f3d394301efbd8b0bb
mellivorines <lilinxi015@163.com>
2023-12-14 20:38:15 +0800

    😃 初始化后台工程

--- mellivorines-blog-admin/vite.config.ts
+++ mellivorines-blog-admin/vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    cors: true,
+    // proxy: {
+    //   '/api': {
+    //     target:,
+    //     changeOrigin: true,
+    //     rewrite: (path) => path.replace(/^\/api/, '')
+    //   }
+    // }
+  },
 })
