236a9f0a0432ce82f6ae2f64bcc149dd965037e9
mellivorines <lilinxi015@163.com>
2023-12-18 19:56:53 +0800

    😃 后台：调整主页

--- mellivorines-blog-admin/src/layouts/dashboard/header/Header.tsx
+++ mellivorines-blog-admin/src/layouts/dashboard/header/Header.tsx
@@ -1,12 +1,12 @@
-import { useTheme } from '@mui/material/styles';
-import { Stack, AppBar, Toolbar, IconButton } from '@mui/material';
-import { bgBlur } from '../../../utils/cssStyles';
+import {useTheme} from '@mui/material/styles';
+import {AppBar, IconButton, Stack, Toolbar} from '@mui/material';
+import {bgBlur} from '../../../utils/cssStyles';
 import useOffSetTop from '../../../hooks/useOffSetTop';
 import useResponsive from '../../../hooks/useResponsive';
-import { HEADER, NAV } from '../../../config-global';
+import {HEADER, NAV} from '../../../config-global';
 import Logo from '../../../components/logo';
 import Iconify from '../../../components/iconify';
-import { useSettingsContext } from '../../../components/settings';
+import {useSettingsContext} from '../../../components/settings';
 import Searchbar from './Searchbar';
 import AccountPopover from './AccountPopover';
 import LanguagePopover from './LanguagePopover';
@@ -15,90 +15,90 @@ import NotificationsPopover from './NotificationsPopover';
 
 
 type Props = {
-  onOpenNav?: VoidFunction;
+    onOpenNav?: VoidFunction;
 };
 
-export default function Header({ onOpenNav }: Props) {
-  const theme = useTheme();
+export default function Header({onOpenNav}: Props) {
+    const theme = useTheme();
 
-  const { themeLayout } = useSettingsContext();
+    const {themeLayout} = useSettingsContext();
 
-  const isNavHorizontal = themeLayout === 'horizontal';
+    const isNavHorizontal = themeLayout === 'horizontal';
 
-  const isNavMini = themeLayout === 'mini';
+    const isNavMini = themeLayout === 'mini';
 
-  const isDesktop = useResponsive('up', 'lg');
+    const isDesktop = useResponsive('up', 'lg');
 
-  const isOffset = useOffSetTop(HEADER.H_DASHBOARD_DESKTOP) && !isNavHorizontal;
+    const isOffset = useOffSetTop(HEADER.H_DASHBOARD_DESKTOP) && !isNavHorizontal;
 
-  const renderContent = (
-    <>
-      {isDesktop && isNavHorizontal && <Logo sx={{ mr: 2.5 }} />}
+    const renderContent = (
+        <>
+            {isDesktop && isNavHorizontal && <Logo sx={{mr: 2.5}}/>}
 
-      {!isDesktop && (
-        <IconButton onClick={onOpenNav} sx={{ mr: 1, color: 'text.primary' }}>
-          <Iconify icon="eva:menu-2-fill" />
-        </IconButton>
-      )}
+            {!isDesktop && (
+                <IconButton onClick={onOpenNav} sx={{mr: 1, color: 'text.primary'}}>
+                    <Iconify icon="eva:menu-2-fill"/>
+                </IconButton>
+            )}
 
-      <Searchbar />
+            <Searchbar/>
 
-      <Stack
-        flexGrow={1}
-        direction="row"
-        alignItems="center"
-        justifyContent="flex-end"
-        spacing={{ xs: 0.5, sm: 1.5 }}
-      >
-        <LanguagePopover />
+            <Stack
+                flexGrow={1}
+                direction="row"
+                alignItems="center"
+                justifyContent="flex-end"
+                spacing={{xs: 0.5, sm: 1.5}}
+            >
 
-        <NotificationsPopover />
+                <NotificationsPopover/>
 
-        <ContactsPopover />
+                <ContactsPopover/>
 
-        <AccountPopover />
-      </Stack>
-    </>
-  );
+                <LanguagePopover/>
+                <AccountPopover/>
+            </Stack>
+        </>
+    );
 
-  return (
-    <AppBar
-      sx={{
-        boxShadow: 'none',
-        height: HEADER.H_MOBILE,
-        zIndex: theme.zIndex.appBar + 1,
-        ...bgBlur({
-          color: theme.palette.background.default,
-        }),
-        transition: theme.transitions.create(['height'], {
-          duration: theme.transitions.duration.shorter,
-        }),
-        ...(isDesktop && {
-          width: `calc(100% - ${NAV.W_DASHBOARD + 1}px)`,
-          height: HEADER.H_DASHBOARD_DESKTOP,
-          ...(isOffset && {
-            height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
-          }),
-          ...(isNavHorizontal && {
-            width: 1,
-            bgcolor: 'background.default',
-            height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
-            borderBottom: `dashed 1px ${theme.palette.divider}`,
-          }),
-          ...(isNavMini && {
-            width: `calc(100% - ${NAV.W_DASHBOARD_MINI + 1}px)`,
-          }),
-        }),
-      }}
-    >
-      <Toolbar
-        sx={{
-          height: 1,
-          px: { lg: 5 },
-        }}
-      >
-        {renderContent}
-      </Toolbar>
-    </AppBar>
-  );
+    return (
+        <AppBar
+            sx={{
+                boxShadow: 'none',
+                height: HEADER.H_MOBILE,
+                zIndex: theme.zIndex.appBar + 1,
+                ...bgBlur({
+                    color: theme.palette.background.default,
+                }),
+                transition: theme.transitions.create(['height'], {
+                    duration: theme.transitions.duration.shorter,
+                }),
+                ...(isDesktop && {
+                    width: `calc(100% - ${NAV.W_DASHBOARD + 1}px)`,
+                    height: HEADER.H_DASHBOARD_DESKTOP,
+                    ...(isOffset && {
+                        height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
+                    }),
+                    ...(isNavHorizontal && {
+                        width: 1,
+                        bgcolor: 'background.default',
+                        height: HEADER.H_DASHBOARD_DESKTOP_OFFSET,
+                        borderBottom: `dashed 1px ${theme.palette.divider}`,
+                    }),
+                    ...(isNavMini && {
+                        width: `calc(100% - ${NAV.W_DASHBOARD_MINI + 1}px)`,
+                    }),
+                }),
+            }}
+        >
+            <Toolbar
+                sx={{
+                    height: 1,
+                    px: {lg: 5},
+                }}
+            >
+                {renderContent}
+            </Toolbar>
+        </AppBar>
+    );
 }
--- mellivorines-blog-admin/src/layouts/dashboard/nav/NavVertical.tsx
+++ mellivorines-blog-admin/src/layouts/dashboard/nav/NavVertical.tsx
@@ -1,108 +1,103 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import { Box, Stack, Drawer } from '@mui/material';
+import {useEffect} from 'react';
+import {useLocation} from 'react-router-dom';
+import {Box, Drawer, Stack} from '@mui/material';
 import useResponsive from '../../../hooks/useResponsive';
-import { NAV } from '../../../config-global';
-import Logo from '../../../components/logo';
+import {NAV} from '../../../config-global';
 import Scrollbar from '../../../components/scrollbar';
-import { NavSectionVertical } from '../../../components/nav-section';
+import {NavSectionVertical} from '../../../components/nav-section';
 import navConfig from './config-navigation';
-import NavDocs from './NavDocs';
 import NavAccount from './NavAccount';
 import NavToggleButton from './NavToggleButton';
 
 
 type Props = {
-  openNav: boolean;
-  onCloseNav: VoidFunction;
+    openNav: boolean;
+    onCloseNav: VoidFunction;
 };
 
-export default function NavVertical({ openNav, onCloseNav }: Props) {
-  const { pathname } = useLocation();
+export default function NavVertical({openNav, onCloseNav}: Props) {
+    const {pathname} = useLocation();
 
-  const isDesktop = useResponsive('up', 'lg');
+    const isDesktop = useResponsive('up', 'lg');
 
-  useEffect(() => {
-    if (openNav) {
-      onCloseNav();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname]);
+    useEffect(() => {
+        if (openNav) {
+            onCloseNav();
+        }
+    }, [pathname]);
 
-  const renderContent = (
-    <Scrollbar
-      sx={{
-        height: 1,
-        '& .simplebar-content': {
-          height: 1,
-          display: 'flex',
-          flexDirection: 'column',
-        },
-      }}
-    >
-      <Stack
-        spacing={3}
-        sx={{
-          pt: 3,
-          pb: 2,
-          px: 2.5,
-          flexShrink: 0,
-        }}
-      >
-        <Logo />
-
-        <NavAccount />
-      </Stack>
-
-      <NavSectionVertical data={navConfig} />
-
-      <Box sx={{ flexGrow: 1 }} />
-
-      <NavDocs />
-    </Scrollbar>
-  );
-
-  return (
-    <Box
-      component="nav"
-      sx={{
-        flexShrink: { lg: 0 },
-        width: { lg: NAV.W_DASHBOARD },
-      }}
-    >
-      <NavToggleButton />
-
-      {isDesktop ? (
-        <Drawer
-          open
-          variant="permanent"
-          PaperProps={{
-            sx: {
-              zIndex: 0,
-              width: NAV.W_DASHBOARD,
-              bgcolor: 'transparent',
-              borderRightStyle: 'dashed',
-            },
-          }}
+    const renderContent = (
+        <Scrollbar
+            sx={{
+                height: 1,
+                '& .simplebar-content': {
+                    height: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                },
+            }}
         >
-          {renderContent}
-        </Drawer>
-      ) : (
-        <Drawer
-          open={openNav}
-          onClose={onCloseNav}
-          ModalProps={{
-            keepMounted: true,
-          }}
-          PaperProps={{
-            sx: {
-              width: NAV.W_DASHBOARD,
-            },
-          }}
+            <Stack
+                spacing={3}
+                sx={{
+                    pt: 3,
+                    pb: 2,
+                    px: 2.5,
+                    flexShrink: 0,
+                }}
+            >
+
+                <NavAccount/>
+            </Stack>
+
+            <NavSectionVertical data={navConfig}/>
+
+            <Box sx={{flexGrow: 1}}/>
+
+        </Scrollbar>
+    );
+
+    return (
+        <Box
+            component="nav"
+            sx={{
+                flexShrink: {lg: 0},
+                width: {lg: NAV.W_DASHBOARD},
+            }}
         >
-          {renderContent}
-        </Drawer>
-      )}
-    </Box>
-  );
+            <NavToggleButton/>
+
+            {isDesktop ? (
+                <Drawer
+                    open
+                    variant="permanent"
+                    PaperProps={{
+                        sx: {
+                            zIndex: 0,
+                            width: NAV.W_DASHBOARD,
+                            bgcolor: 'transparent',
+                            borderRightStyle: 'dashed',
+                        },
+                    }}
+                >
+                    {renderContent}
+                </Drawer>
+            ) : (
+                <Drawer
+                    open={openNav}
+                    onClose={onCloseNav}
+                    ModalProps={{
+                        keepMounted: true,
+                    }}
+                    PaperProps={{
+                        sx: {
+                            width: NAV.W_DASHBOARD,
+                        },
+                    }}
+                >
+                    {renderContent}
+                </Drawer>
+            )}
+        </Box>
+    );
 }
